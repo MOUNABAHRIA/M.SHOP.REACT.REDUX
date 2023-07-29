0d5c62c7fee056c9b9f4826cf2375a58eeb91802
Mouna bahria <mounabahria5@gmail.com>
2023-07-29 17:33:51 +0200

    first

---
+++ src/pages/service/ServiceData.js
@@ -0,0 +1,32 @@
+
+import { FaShippingFast } from "react-icons/fa";
+import {FaAmazonPay } from "react-icons/fa";
+import {FaArrowCircleLeft } from "react-icons/fa";
+
+
+export const ServiceData=[
+   
+    
+    {
+        icon:< FaShippingFast />,
+        title: "Free Shipping",
+        subtitle: "Lorem ipsum dolor sit amet.",
+        bg: "#fdefe6",
+      },
+{
+    
+    icon: <FaArrowCircleLeft />,
+    title: "Easy Returns",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#ceebe9",
+      
+
+},{
+    
+    icon: < FaAmazonPay />,
+    title: "Secure Payment",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#e2f2b2",
+      
+
+},]
